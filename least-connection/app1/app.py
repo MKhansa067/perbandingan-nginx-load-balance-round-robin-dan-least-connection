e26a7133544a1b0c06d8da438b7632d503f8db31
Muhamad Khansa Khalifaturohman <105094399+MKhansa067@users.noreply.github.com>
2026-05-08 16:10:15 +0700

    Update app.py

--- least-connection/app1/app.py
+++ least-connection/app1/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+#import time
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 
     print("REQUEST MASUK KE APP1")
-
+    
+    #time sleep(2)
+    #time sleep(5)
     return "Response from APP 1"
 
 app.run(host='0.0.0.0', port=5000)
