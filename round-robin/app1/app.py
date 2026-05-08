c726f13412b664e2dbfb41c243bc8fb4341ca7c9
Muhamad Khansa Khalifaturohman <105094399+MKhansa067@users.noreply.github.com>
2026-05-08 14:48:51 +0700

    Update app.py

--- round-robin/app1/app.py
+++ round-robin/app1/app.py
@@ -1,16 +1,11 @@
-# skenario delay 2 detik
 from flask import Flask
-import time
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 
     print("REQUEST MASUK KE APP1")
-    # simulasi server lambat
-    time.sleep(2)
 
-    return "Response from APP 1 (DELAY 2s)"
+    return "Response from APP 1"
 
 app.run(host='0.0.0.0', port=5000)
