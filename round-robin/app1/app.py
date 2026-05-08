7e9b812945a014e58261d91e00ddd3f3df73d968
Muhamad Khansa Khalifaturohman <105094399+MKhansa067@users.noreply.github.com>
2026-05-08 16:12:32 +0700

    Update app.py

--- round-robin/app1/app.py
+++ round-robin/app1/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+#import time
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +7,8 @@ def home():
 
     print("REQUEST MASUK KE APP1")
 
+    #time sleep(2)
+    #time sleep(5)
     return "Response from APP 1"
 
 app.run(host='0.0.0.0', port=5000)
